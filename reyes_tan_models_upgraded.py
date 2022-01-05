5e2843f4df3b8b6e46b0fab678ba56c9e6ed91f7
Sam@MSI <tansamuel852@gmail.com>
2022-01-06 01:00:38 +0800

    Typo Fix

--- reyes_tan_models_upgraded.py
+++ reyes_tan_models_upgraded.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
-from utilities.eyes_tan_utilities import *
+from utilities.reyes_tan_utilities import *
 
 from src.dataset.urmp.urmp_sample import *
 from src.utils.multiEpochsDataLoader import MultiEpochsDataLoader as DataLoader
